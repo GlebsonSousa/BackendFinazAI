760f52654022ec41ffc2cdd51d3d6cd9eba232e7
Glebson <glebsonsalmeida@gmail.com>
2025-07-12 17:53:55 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();  // Carrega variáveis de ambiente do .env
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get ("/", async (req, res) => {
+    return res.status(200).json('Api rodando!')
+})
+
+app.post ("/recebemensagem", async (req, res) => {
+    const { numero, mensagem } = req.body;
+
+    return res.status(200).json({
+        numero,
+        mensagem,
+    })
+})
+
+app.listen(PORT, () => {
+  console.log(`🚀 API da FinanzAI rodando em http://localhost:${PORT}`);
+});
